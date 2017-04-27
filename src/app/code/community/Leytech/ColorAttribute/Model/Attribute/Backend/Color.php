c87fa3d629fa7fea44da440594b716c72fa41f35
Chris Nolan <chris@leytech.co.uk>
2017-04-27 20:21:01 +0100

    Copyright and license changes

--- src/app/code/community/Leytech/ColorAttribute/Model/Attribute/Backend/Color.php
+++ src/app/code/community/Leytech/ColorAttribute/Model/Attribute/Backend/Color.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * Backend model for color input type
- *
- * @category Leytech
- * @package  Leytech_ColorAttribute
- * @author   Chris Nolan <chris@leytech.co.uk>
+ * @package    Leytech_ColorAttribute
+ * @author     Chris Nolan (chris@leytech.co.uk)
+ * @copyright  Copyright (c) 2017 Leytech
+ * @license    https://opensource.org/licenses/MIT  The MIT License  (MIT)
  */
 class Leytech_ColorAttribute_Model_Attribute_Backend_Color extends Mage_Eav_Model_Entity_Attribute_Backend_Abstract
 {
